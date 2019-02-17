c8c7ef35e0f83d849047d30bcf553c2ae99c3b82
Jon Robison <narfman0@gmail.com>
2019-02-17 14:55:40 -0500

    -Update viewsets slightly to remove methods we dont want exposed yet in the api

--- kidcorral/api/views.py
+++ kidcorral/api/views.py
@@ -1,12 +1,21 @@
-from rest_framework import generics, mixins, viewsets
+from rest_framework import mixins, viewsets
 
 from kidcorral.api import serializers
 from kidcorral.family.models import Family
 from kidcorral.person.models import Person
 from kidcorral.visit.models import Visit
 
+# TODO: i am quite sure for each of these viewsets, an attacker can perform
+# create/updates without authz, as there are no permissions classes. While
+# they cannot see the information, invalid writes are still bad, so revisit.
 
-class FamilyViewSet(viewsets.ModelViewSet):
+
+class FamilyViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = Family.objects.none()
     serializer_class = serializers.FamilySerializer
 
@@ -16,7 +25,12 @@ class FamilyViewSet(viewsets.ModelViewSet):
         return self.request.user.family_legal_guardians.all()
 
 
-class PersonViewSet(viewsets.ModelViewSet):
+class PersonViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = Person.objects.none()
     serializer_class = serializers.PersonSerializer
 
@@ -30,14 +44,18 @@ class PersonViewSet(viewsets.ModelViewSet):
         return people
 
 
-class VisitViewSet(viewsets.ModelViewSet):
+class VisitViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = Visit.objects.none()
     serializer_class = serializers.VisitSerializer
 
     def get_queryset(self):
         if self.request.user.is_staff:
             return Visit.objects.all()
-        # initially we respond with all visits for which we are originator
         # eventually we will respond with all children's visits for which we
         # are legal guardian
         return self.request.user.visit_originators.all()
