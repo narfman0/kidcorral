9ca00c6ea2d3deaae60fb8f217d9cfd3fa49edcd
Jon Robison <narfman0@gmail.com>
2019-02-18 15:15:51 -0500

    -Add login specifiers

--- kidcorral/fe/views.py
+++ kidcorral/fe/views.py
@@ -1,5 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
+@login_required
 def index(request):
-    return render(request, "index.html", context={})
+    guardians = request.user.family_legal_guardians.all()
+    children = request.user.children.all()
+    return render(
+        request, "index.html", context={"guardians": guardians, "children": children}
+    )
