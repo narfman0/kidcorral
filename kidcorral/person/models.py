3056857e562f92898ddde3f47ddd047ecf7f6229
Jon Robison <narfman0@gmail.com>
2019-03-01 16:53:00 -0500

    -Change to charfield for phone, the other was too aggressive

--- kidcorral/person/models.py
+++ kidcorral/person/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Person(AbstractUser):
     allergies = models.TextField(null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
-    phone_number = PhoneNumberField(null=True, blank=True)
+    phone_number = models.CharField(null=True, blank=True, max_length=64)
     phone_preferred = models.BooleanField(default=True)
     volunteer = models.BooleanField(default=False)
 
